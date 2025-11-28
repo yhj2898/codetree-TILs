95b09d1cdd92f51a37cc85ee1bbc1ddca2da00c9
yhj2898 <yhj2898@naver.com>
2025-11-28 21:05:06 +0900

    Title: 출석 부르기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251128/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    n=int(input())
+    if n>=5:
+        print('Vacancy')
+        break
+    if n==1:
+        print('John')
+    elif n==2:
+        print('Tom')
+    elif n==3:
+        print('Paul')
+    else:
+        print('Sam')
