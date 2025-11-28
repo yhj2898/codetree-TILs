fd4673af3b9b4ee8ff2bf7733729b0b89ad2ccde
yhj2898 <yhj2898@naver.com>
2025-11-28 18:20:54 +0900

    Title: 369 게임, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/369 게임/369-game.py
+++ 251128/369 게임/369-game.py
@@ -4,9 +4,11 @@ i=1
 while i<=n:
     if i%3==0:
         print(0, end=' ')
-    elif  (i%10==3) or (i%10==6) or (i%10==9):
+    elif  (i//10==3) or (i//10==6) or (i//10==9):
         print(0, end=' ')
-    elif  (i%100==3) or (i%100==6) or (i%100==9):
+    elif  (i//100==3) or (i%100==3):
+        print(0, end=' ')
+    elif (i%10==3) or (i%10==6) or (i%10==9):
         print(0, end=' ')
     else:
         print(i, end=' ')
