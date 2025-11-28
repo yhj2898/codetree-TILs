c5f4a93af012a89ea8be630a25ea78a8bb28c152
yhj2898 <yhj2898@naver.com>
2025-11-28 15:39:41 +0900

    Title: 두 사람, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/두 사람/two-person.py
+++ 251128/두 사람/two-person.py
@@ -3,7 +3,7 @@ age2, sex2 = input().split()
 age1 = int(age1)
 age2 = int(age2)
 
-if (age1>=19 and sex1=='M')or (age2>=10 and sex2=='M'):
+if (age1>=19 and sex1=='M')or (age2>=19 and sex2=='M'):
     print(1)
 else: 
     print(0)
