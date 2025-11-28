43e227714e6d3399275575922e60606e59c2afbe
yhj2898 <yhj2898@naver.com>
2025-11-28 19:31:51 +0900

    Title: 완전수 판별기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/완전수 판별기/perfect-number-discriminator.py
+++ 251128/완전수 판별기/perfect-number-discriminator.py
@@ -1,9 +1,10 @@
 n = int(input())
 
-prime=[]
-
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        if i%j==0:
-    prime.append(i)
-print(prime)
+total=0
+for i in range(1,n):
+    if n%i==0:
+        total+=i
+if total == n:
+    print('P')
+else:
+    print('N')
