f95eaec4fec42e223556ecc416c581979799aacb
yhj2898 <yhj2898@naver.com>
2025-11-28 19:55:10 +0900

    Title: 조건에 부합하는 수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251128/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+for i in range(1,a+1):
+    if i%2==0 and i%4 != 0:
+        continue
+    elif (i//8)%2==0:
+        continue
+    elif i%7<4:
+        continue
+    print(i, end=' ')
