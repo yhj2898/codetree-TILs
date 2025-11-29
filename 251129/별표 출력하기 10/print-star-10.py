500cec1cbd75240ddd3f2abae3c7ae6aa18fc456
yhj2898 <yhj2898@naver.com>
2025-11-29 16:26:57 +0900

    Title: 별표 출력하기 10, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251129/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,10 @@
+n=int(input())
+
+for i in range(1,2*n+1):
+    if i%2==1:
+        for j in range(i//2+1):
+            print('*', end=' ')
+    else:
+        for j in range(n-(i//2)+1):
+            print('*', end=' ')
+    print()
