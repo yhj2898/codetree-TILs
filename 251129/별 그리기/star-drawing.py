994e88ed36c08cbb4ccb68b8985e2ab0df4b8874
yhj2898 <yhj2898@naver.com>
2025-11-29 15:50:05 +0900

    Title: 별 그리기, Time: 72ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 251129/별 그리기/star-drawing.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+# Please write your code here.
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ', end='')
+    for j in range(2*i+1):
+        print('*',end='')
+    print()
+
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end='')
+    for j in range(2*n-2*i-3):
+        print('*',end='')
+    print()
