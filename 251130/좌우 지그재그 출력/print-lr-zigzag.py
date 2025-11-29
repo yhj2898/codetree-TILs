c8077ec3d796d716bd833f927479281315d8d2cb
yhj2898 <yhj2898@naver.com>
2025-11-30 01:15:04 +0900

    Title: 좌우 지그재그 출력, Time: 74ms, Memory: 18MB, Status: Passed - Codetree

--- 251130/좌우 지그재그 출력/print-lr-zigzag.py
+++ 251130/좌우 지그재그 출력/print-lr-zigzag.py
@@ -7,6 +7,6 @@ for i in range(1,n+1):
             cnt+=1
     else:
         for j in range(1,n+1):
-            print(input*n+1-j, end=' ')
+            print(i*n+1-j, end=' ')
             cnt+=1
     print()
