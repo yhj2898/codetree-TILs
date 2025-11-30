e4fc0032514e822f9217da95651a95d07baaeef6
yhj2898 <yhj2898@naver.com>
2025-11-30 18:21:32 +0900

    Title: 알파벳 출력하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251130/알파벳 출력하기/print-alphabet.py
+++ 251130/알파벳 출력하기/print-alphabet.py
@@ -1,9 +1,11 @@
 n=int(input())
 
 x=ord('A')
-
 for i in range(n):
     for j in range(i+1):
         print(chr(x), end='')
         x+=1
+        if x==91:
+            x=ord('A')
+    
     print()
