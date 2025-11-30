06927689787e2f282ac2e47b4514351ea8cf0bf0
yhj2898 <yhj2898@naver.com>
2025-11-30 22:30:40 +0900

    Title: N개의 숫자 중 최소, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251130/N개의 숫자 중 최소/min-of-n-num.py
+++ 251130/N개의 숫자 중 최소/min-of-n-num.py
@@ -7,5 +7,9 @@ min_val = 999
 for i in a:
     if i < min_val:
         min_val = i
+cnt=0
+for i in a:
+    if i == min_val:
+        cnt += 1
 
-print(min_val, a.count(min_val))
+print(min_val, cnt)
