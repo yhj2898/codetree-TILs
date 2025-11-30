7f5fde950947f7a88e1de476fe56fa0ebaebc6c5
yhj2898 <yhj2898@naver.com>
2025-11-30 18:26:37 +0900

    Title: 알파벳 출력하기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251130/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,13 @@
+n=int(input())
+
+x=ord('A')
+
+for i in range(n):
+    for j in range(i):
+        print(' ', end=' ')
+    for j in range(n-i):
+        print(chr(x), end=' ')
+        x+=1
+        if x==ord('Z'):
+            x=ord('A')
+    print()
