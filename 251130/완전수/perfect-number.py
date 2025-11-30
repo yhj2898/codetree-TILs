3bfa01c3ad9a26490d5508e783cb4882e3fcc2c1
yhj2898 <yhj2898@naver.com>
2025-11-30 18:43:22 +0900

    Title: 완전수, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 251130/완전수/perfect-number.py
+++ 251130/완전수/perfect-number.py
@@ -1,9 +1,9 @@
 start, end = map(int, input().split())
 
 # Please write your code here.
-total=0
 cnt=0
 for i in range(start, end+1):
+    total=0
     for j in range(1,i):
         if i%j==0:
             total+=j
