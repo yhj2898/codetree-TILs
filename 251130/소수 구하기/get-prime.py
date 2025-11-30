b164f8557bab6d37fcf476cc19849078b346ab8f
yhj2898 <yhj2898@naver.com>
2025-11-30 19:01:23 +0900

    Title: 소수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251130/소수 구하기/get-prime.py
@@ -0,0 +1,11 @@
+n=int(input())
+
+
+for i in range(2,n+1):
+    is_prime=True
+    for j in range(2,i):
+        if i%j==0:
+            is_prime=False
+            break
+    if is_prime:
+        print(i, end=' ')
