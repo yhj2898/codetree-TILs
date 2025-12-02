d78a0a9fcf03aab3ee6242e01664868a36398c15
yhj2898 <yhj2898@naver.com>
2025-12-03 00:41:09 +0900

    Title: 문자열 놀이, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251203/문자열 놀이/play-with-string.py
+++ 251203/문자열 놀이/play-with-string.py
@@ -1,7 +1,19 @@
 s,q = input().split()
+s = list(s)
 q = int(q)
 
 for _ in range(q):
-    a = input().split()
-    if a[0]=='1':
-        print(s[a[0]])
+    ques = input().split()
+
+    if ques[0] == '1':
+        a = int(ques[1]) -1
+        b = int(ques[2]) -1
+        s[a], s[b] = s[b], s[a]
+
+    elif ques[0] == '2':
+        x = ques[1]
+        y = ques[2]
+        for i in range(len(s)):
+            if s[i]==x:
+                s[i]=y
+    print(''.join(s))
