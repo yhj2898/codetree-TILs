deb823261c03e13a72dd8aabd71cad95178e6565
yhj2898 <yhj2898@naver.com>
2025-12-03 23:35:11 +0900

    Title: 숫자로 이루어진 사각형, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 251203/숫자로 이루어진 사각형/rectangle-with-a-number.py
+++ 251203/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -4,16 +4,12 @@ n = int(input())
 def rec(n):
     arr = [[0 for _ in range(n)] for _ in range(n)]
     cnt=1
-    
-    for i in arr:
-        for j in range(n):
-            i[j]=cnt
+    for _ in range(n):
+        for _ in range(n):
+            print(cnt, end=' ')
             cnt+=1
             if cnt==10:
                 cnt=1
-    
-    for i in arr:
-        for j in i:
-            print(j, end=' ')
         print()
+
 rec(n)
