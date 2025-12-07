00f8dd35b66a90b38c416040b373cbe9baf8e28c
yhj2898 <yhj2898@naver.com>
2025-12-07 16:12:55 +0900

    Title: 두 개의 동일한 수열, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251207/두 개의 동일한 수열/two-equal-series.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+# Please write your code here.
+A.sort()
+B.sort()
+
+if A==B:
+    print('Yes')
+else:
+    print('No')
