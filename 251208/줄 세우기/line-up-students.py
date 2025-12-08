47fd293b19ce3247471d74aa749d12a7e09c4679
yhj2898 <yhj2898@naver.com>
2025-12-08 23:51:51 +0900

    Title: 줄 세우기, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251208/줄 세우기/line-up-students.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+# Please write your code here.
+class Student:
+    def __init__(self, h, w, no):
+        self.h, self.w, self.no = h, w, no
+
+students=[]
+for i in range(n):
+    h, w = map(int,input().split())
+    students.append(Student(h,w,_))
+
+students.sort(key=lambda x: (-x.h, -x.w, x.no))
+
+for i in students:
+    print(i.h, i.w, i.no+1)
