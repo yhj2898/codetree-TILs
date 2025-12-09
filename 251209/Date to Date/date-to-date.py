033bf9a574bee395388a7e35a61a6fe57e22da8a
yhj2898 <yhj2898@naver.com>
2025-12-09 17:08:39 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251209/Date to Date/date-to-date.py
@@ -0,0 +1,17 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+DOM = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+days = 1
+
+while True:
+    if m1==m2 and d1 == d2:
+        break
+    days +=1
+    d1+=1
+
+    if d1 > DOM[m1]:
+        m1+=1
+        d1=1
+
+print(days)
