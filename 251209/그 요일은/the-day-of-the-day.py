8ec15d8a0a48c254fc6ff5d73775a151e1973706
yhj2898 <yhj2898@naver.com>
2025-12-09 18:04:16 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251209/그 요일은/the-day-of-the-day.py
+++ 251209/그 요일은/the-day-of-the-day.py
@@ -3,7 +3,7 @@ A = input()
 
 # Please write your code here.
 def days(m,d):
-    DOM=[0,31,29,31,3,0,31,30,31,31,30,31,30,31]
+    DOM=[0,31,29,31,30,31,30,31,31,30,31,30,31]
     total_days=0
 
     for i in range(m):
@@ -12,4 +12,10 @@ def days(m,d):
     return total_days
 
 day_diff = days(m2,d2) - days(m1,d1)
-print(day_diff//7+1)
+weekday=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+idx=0
+for i in range(7):
+    if weekday[i] == A:
+        idx = i
+
+print((days(m2,d2) - (days(m1,d1)+idx))//7 +1)
