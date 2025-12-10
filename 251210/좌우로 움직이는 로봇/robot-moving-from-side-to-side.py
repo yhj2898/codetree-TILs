e014c1df0555ccf8e6b855e2f813b91d21abb94e
yhj2898 <yhj2898@naver.com>
2025-12-10 18:12:49 +0900

    Title: 좌우로 움직이는 로봇, Time: 176ms, Memory: 50MB, Status: Passed - Codetree

--- 251210/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 251210/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -8,27 +8,24 @@ time_a = 1
 for _ in range(n):
     t, d = input().split()
     for _ in range(int(t)):
-        if d=='R':
-            a[time_a] = a[time_a-1] +1
-            time_a+=1
-        else:
-            a[time_a] = a[time_a-1] -1
-            time_a+=1
+        a[time_a] = a[time_a -1] + (1 if d=='R' else -1)
+        time_a+=1
+
 time_b = 1
 for _ in range(m):
     t, d = input().split()
     for _ in range(int(t)):
-        if d=='R':
-            b[time_b] = b[time_b-1] +1
-            time_b+=1
-        else:
-            b[time_b] = b[time_b-1] -1
-            time_b+=1
+        b[time_b] = b[time_b -1] + (1 if d=='R' else -1)
+        time_b +=1
+if time_a<time_b:
+    for i in range(time_a,time_b):
+        a[i]=a[i-1]
+elif time_a>time_b:
+    for i in range(time_b,time_a):
+        b[i]=b[i-1]
 
-ans=0
-for i in range(len(a)):
-    if a[i]==b[i] and a[i-1]!=b[i-1]:
-        ans+=1
-print(ans)
-
-
+cnt=0
+for i in range(1, max(time_a, time_b)):
+    if a[i]==b[i] and a[i-1] != b[i-1]:
+        cnt+=1
+print(cnt)
