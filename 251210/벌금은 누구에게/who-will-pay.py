3b6b3f846c82c22e1b343c5ff628ded6390b2d0f
yhj2898 <yhj2898@naver.com>
2025-12-10 17:20:46 +0900

    Title: 벌금은 누구에게, Time: 70ms, Memory: 16MB, Status: Passed - Codetree

--- 251210/벌금은 누구에게/who-will-pay.py
+++ 251210/벌금은 누구에게/who-will-pay.py
@@ -3,9 +3,13 @@ student = [int(input()) for _ in range(M)]
 
 # Please write your code here.
 answer = [0 for _ in range(N+1)]
+found = False
 
 for s in student:
     answer[s]+=1
-    if answer[s]>=3:
+    if answer[s]>=K:
         print(s)
+        found = True
         break
+if found == False:
+    print(-1)
