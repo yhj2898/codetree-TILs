2addd37cfc3c9a31cf7d0e1df583c7164185f892
yhj2898 <yhj2898@naver.com>
2025-12-10 20:17:37 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251210/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 251210/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,20 +1,35 @@
-N, K, P, T = map(int, input().split())
-handshakes = [tuple(map(int, input().split())) for _ in range(T)]
+class Shake:
+    def __init__(self, t, p1, p2):
+        self.t, self.p1, self.p2 = t, p1, p2
 
-# Please write your code here.
-handshakes.sort(key=lambda x: x[0])
-inf = [0 for _ in range(N+1)]
-inf[P] = 1
-cnt=0
-for t, x, y in handshakes:
-    if x==P:
-        inf[y] = 1
-        cnt+=1
-    elif y==P:
-        inf[x] =1
-        cnt+=1
-    if cnt>=2:
-        break
+n, k, p, t = map(int, input().split())
+shakes=[]
+for _ in range(t):
+    t, p1, p2 = map(int, input().split())
+    shakes.append(Shake(t, p1, p2))
 
-for i in range(1,N+1):
-    print(inf[i], end='')
+shake_num=[0] * (n+1)
+infected = [False] * (n+1)
+infected[p] = True
+
+shakes.sort(key=lambda x: x.t)
+
+for s in shakes:
+    target1 = s.p1
+    target2 = s.p2
+
+    if infected[target1]:
+        shake_num[target1]+=1
+    if infected[target2]:
+        shake_num[target2]+=1
+
+    if shake_num[target1]<=k and infected[target1]:
+        infected[target2] = True
+    if shake_num[target2]<=k and infected[target2]:
+        infected[target1] = True
+
+for i in range(1, n+1):
+    if infected[i]:
+        print(1, end='')
+    else:
+        print(0, end='')
