aaf5717884a3c240feb783c8f90222dfdd733cbc
yhj2898 <yhj2898@naver.com>
2025-12-13 14:43:49 +0900

    Title: 체크판위에서 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251213/체크판위에서 2/on-the-checkboard-2.py
+++ 251213/체크판위에서 2/on-the-checkboard-2.py
@@ -2,8 +2,8 @@ r,c = map(int, input().split())
 a = [list(input().split()) for _ in range(r)]
 
 cnt=0
-for i in range(1,r-2):
-    for j in range(1,c-2):
+for i in range(1,r):
+    for j in range(1,c):
         for k in range(i+1, r-1):
             for l in range(j+1,c-1):
                 if a[0][0] != a[i][j] and a[i][j] != a[k][l] and a[k][l]!=a[r-1][c-1]:
