b1188d5d9e1566a686bcecedde2fc612af1e18d3
yhj2898 <yhj2898@naver.com>
2025-12-14 22:29:26 +0900

    Title: 씨 오 더블유 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251214/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+s = input()
+
+cnt=0
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if s[i]=='C' and s[j]=='O' and s[k]=='W':
+                cnt+=1
+print(cnt)
