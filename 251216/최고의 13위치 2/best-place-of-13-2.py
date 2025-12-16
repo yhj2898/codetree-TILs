7149c194d4e7745278c6976c0680df48ff37d08a
yhj2898 <yhj2898@naver.com>
2025-12-16 21:05:59 +0900

    Title: 최고의 13위치 2, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251216/최고의 13위치 2/best-place-of-13-2.py
+++ 251216/최고의 13위치 2/best-place-of-13-2.py
@@ -7,7 +7,7 @@ for i in range(n):
         for k in range(i,n):
             for l in range(n-2):
                 if i==k:
-                    if j==l:
+                    if j<l+2 or l<j+2:
                         continue
                 max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[k][l] + arr[k][l+1] + arr[k][l+2])
 print(max_cnt)
