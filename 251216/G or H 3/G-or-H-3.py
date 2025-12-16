6855ce4d9c618b4177c776f2fb67927994a1b6a1
yhj2898 <yhj2898@naver.com>
2025-12-16 22:19:44 +0900

    Title: G or H 3, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251216/G or H 3/G-or-H-3.py
@@ -0,0 +1,21 @@
+n, k = map(int, input().split())
+x = []
+c = []
+for _ in range(n):
+    pos, char = input().split()
+    x.append(int(pos))
+    c.append(char)
+
+answer = [0]*10001
+for i in range(n):
+    if c[i]=='G':
+        answer[x[i]]=1
+    else:
+        answer[x[i]]=2
+max_score = 0
+for i in range(1,10000-k+2):
+    score=0
+    for j in range(i,i+k+1):
+        score+=answer[j]
+    max_score = max(max_score,score)
+print(max_score)
