005331f53fc5ce65222384efd4c1a3629e6e2b21
yhj2898 <yhj2898@naver.com>
2025-12-18 23:10:14 +0900

    Title: 개발자의 능력 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251218/개발자의 능력 3/ability-of-developer-3.py
+++ 251218/개발자의 능력 3/ability-of-developer-3.py
@@ -8,7 +8,7 @@ def diff(a,b,c):
 min_diff=999
 
 for i in range(6):
-    for j in range(6):
-        for k in range(6):
+    for j in range(i+1,6):
+        for k in range(j+1,6):
             min_diff = min(min_diff, diff(i,j,k))
 print(min_diff)
