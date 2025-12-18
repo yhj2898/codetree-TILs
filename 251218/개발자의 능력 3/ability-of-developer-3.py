76f15a020e1889a8484633af3414c0f27e1303d1
yhj2898 <yhj2898@naver.com>
2025-12-18 23:09:09 +0900

    Title: 개발자의 능력 3, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251218/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,14 @@
+x = list(map(int, input().split()))
+
+def diff(a,b,c):
+    sum1 = x[a] + x[b] + x[c]
+    sum2 = sum(x) - sum1
+    return abs(sum1-sum2)
+
+min_diff=999
+
+for i in range(6):
+    for j in range(6):
+        for k in range(6):
+            min_diff = min(min_diff, diff(i,j,k))
+print(min_diff)
