d8c4617c542c83a8ac5689faf40a39faad2f4a62
yhj2898 <yhj2898@naver.com>
2025-12-21 21:54:15 +0900

    Title: 겹치지 않는 선분 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251221/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 251221/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,11 +1,16 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 
+ans=0
+
 for i, (x1,y1) in enumerate(lines):
-    cnt=0
+    ok = True
     for j, (x2,y2) in enumerate(lines):
         if x1==x2 and y1==y2:
             continue
-        if (x1-x2)*(y1-y2)>0:
-            cnt+=1
-print(cnt)
+        if (x1-x2)*(y1-y2)<0:
+            ok = False
+            break
+    if ok:
+        ans+=1
+print(ans)
