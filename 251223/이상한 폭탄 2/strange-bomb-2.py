73d6b5753027af1b538b5c0cfb2b769449871acb
yhj2898 <yhj2898@naver.com>
2025-12-23 22:27:06 +0900

    Title: 이상한 폭탄 2, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 251223/이상한 폭탄 2/strange-bomb-2.py
+++ 251223/이상한 폭탄 2/strange-bomb-2.py
@@ -2,15 +2,14 @@ n,k = map(int, input().split())
 num = [int(input()) for _ in range(n)]
 
 ans=-1
-
+idx=[]
 for i in range(n):
-    idx = []
-
+    idx=[i]
     for j in range(i+1,n):
         if num[j]==num[i]:
             idx.append(j)
     if not idx:
         continue
-    if idx[-1] - idx[0] <=k:
+    if len(idx)>=2 and idx[-1] - idx[0] <=k:
         ans = max(ans, num[i])
 print(ans)
