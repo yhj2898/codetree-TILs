5c24ae656c08d224e6fe7f72e4b8adc23057879f
yhj2898 <yhj2898@naver.com>
2025-12-24 18:09:26 +0900

    Title: 빙산의 일각 2, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 251224/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 251224/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -3,14 +3,9 @@ h = [int(input()) for _ in range(n)]
 
 ans=0
 for k in range(1001):
-    arr=[0]*(n+1)
     cnt=0
     for i in range(n):
-        arr[i] = 1 if h[i]>k else -1
-        
-    for i in range(n-1):
-        if arr[i]*arr[i+1]==-1:
+        if h[i]>k and (i==0 or h[i-1]<=k):
             cnt+=1
-    ans = max(ans, cnt//2+1)
+    ans=max(ans,cnt)
 print(ans)
-    
